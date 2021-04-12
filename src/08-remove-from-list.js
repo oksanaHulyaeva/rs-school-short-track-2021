86c13f2d135bec3493088b8dddb3f38648ab8821
oksanaHulyaeva <hulyaevaoksana@gmail.com>
2021-04-12 19:49:53 +0300

    feat: implement remove from list

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -17,8 +17,18 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new Error('Not implemented');
+function removeKFromList(l, k) {
+  let list = l;
+
+  while (list.next) {
+    if (list.value === k) {
+      list.value = list.next.value;
+      list.next = list.next.next;
+    } else {
+      list = list.next;
+    }
+  }
+  return l;
 }
 
 module.exports = removeKFromList;
